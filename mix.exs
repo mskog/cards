3a0178cf0adc2b62e7155f1187f05442f8757bbc
Magnus Skog <magnus.m.skog@gmail.com>
2017-01-11 13:20:11 +0100

    Elixir bootcamp day 2

--- mix.exs
+++ mix.exs
@@ -28,6 +28,6 @@ defmodule Cards.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:mix_test_watch, "~> 0.2", only: :dev}]
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,6 +2,9 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
+  test "contains?" do
+    deck = Cards.create_deck
+    assert Cards.contains?(deck, "Ace of Hearts") == true
+    assert Cards.contains?(deck, "King of Clubs") == false
   end
 end
