f233855396cc4f3d91a5408673ac722085b1eedc
Magnus Skog <magnus.m.skog@gmail.com>
2017-01-12 19:59:56 +0100

    Elixir bootcamp day 3

--- mix.exs
+++ mix.exs
@@ -28,6 +28,9 @@ defmodule Cards.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:mix_test_watch, "~> 0.2", only: :dev}]
+    [
+      {:ex_doc, "~> 0.12"},
+      {:mix_test_watch, "~> 0.2", only: :dev}
+    ]
   end
 end
