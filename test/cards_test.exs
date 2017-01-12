6e7621d747ecfe18d29dbfd745b333ef2b5dc9f6
Magnus Skog <magnus.m.skog@gmail.com>
2017-01-12 08:38:32 +0100

    Elixir bootcamp day 2 continued

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -7,4 +7,11 @@ defmodule CardsTest do
     assert Cards.contains?(deck, "Ace of Hearts") == true
     assert Cards.contains?(deck, "King of Clubs") == false
   end
+
+  test "deal" do
+    deck = Cards.create_deck
+    {hand, rest_of_deck} = Cards.deal(deck, 5)
+    assert length(hand) == 5
+    assert length(rest_of_deck) == length(deck) - 5
+  end
 end
